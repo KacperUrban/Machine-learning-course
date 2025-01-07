ca91645a271a707259db751764bb85f64ab64083
Kacper Urban <urbankacper613@gmail.com>
2025-01-07 19:21:31 +0100

    Implemented a preprocessing data for classification and regression

--- notebooks/asses_model.py
+++ notebooks/asses_model.py
@@ -1,8 +1,10 @@
 from sklearn.metrics import accuracy_score, f1_score
 import pandas as pd
+import os
+import numpy as np
 
-y_pred = pd.read_csv("wyniki_klasyfikacji.csv")
-y_test = pd.read_csv("test_wyniki.csv")
+y_pred = pd.read_csv(os.path.join("results", "wyniki_klasyfikacji.csv"))
+y_test = pd.read_csv(os.path.join("data", "test_pokemon.csv"))
 
-print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-print(f"F1 score: {f1_score(y_test, y_pred, average='micro')}")
+print(f"Accuracy: {np.round(accuracy_score(y_test, y_pred), 4)}")
+print(f"F1 score: {np.round(f1_score(y_test, y_pred, average='micro'), 4)}")
